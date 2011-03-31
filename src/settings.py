4d4aebe5365c39b6c6ced636e3190bc290ca856b
Bhargav Nookala <nooknb@gmail.com>
2011-03-31 18:11:03 -0500

    Abstracted handlers to library

---
+++ src/settings.py
@@ -0,0 +1,8 @@
+
+#Root URL of acoustics instance
+ROOT = "https://www-s.acm.uiuc.edu/acoustics/"
+
+#Other state URLs
+URLS = {"state": ROOT + "json.pl",
+		"search": ROOT + "json.pl?mode=search",
+		}
